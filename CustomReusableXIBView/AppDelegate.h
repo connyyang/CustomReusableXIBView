c59b2f997e596f1175f26c2bdd4133de3c817184
Conny Yang <connyyang097@gmail.com>
2017-02-02 10:51:20 +1100

    Initial Commit

---
+++ CustomReusableXIBView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomReusableXIBView
+//
+//  Created by Conny Yang on 2/02/2017.
+//  Copyright © 2017 Conny Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
